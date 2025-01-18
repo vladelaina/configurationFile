accdb7cd3418b268f95cf307abd9fa2d21e890fb
vladelaina <vladelaina@gmail.com>
2025-01-18 11:46:06 +0800

    添加了切换图片

--- wezterm.lua
+++ wezterm.lua
@@ -1,64 +1,64 @@
 local wezterm = require("wezterm")
 
-local BACKGROUND_IMAGE = "3.jpg"
+local BACKGROUND_IMAGE = "95.jpg"
 local WSL_HOME = "/home/vladelaina"
 
 local config = {
-    font_size = 22,
-    font = wezterm.font("Fira Code"),
-    window_decorations = "RESIZE",
-    color_scheme = "Catppuccin Mocha",
-    text_background_opacity = 0.8,
-    window_background_opacity = 0.8,
-    use_fancy_tab_bar = false,
-    automatically_reload_config = true,
-    hide_tab_bar_if_only_one_tab = true,
-    show_new_tab_button_in_tab_bar = false,
-    adjust_window_size_when_changing_font_size = false,
-    default_cursor_style = "BlinkingBar",
-    window_close_confirmation = "NeverPrompt",
-    window_padding = {
-        left = 35,
-        right = 30,
-        top = 1,
-        bottom = 20,
+  font_size = 20,
+  font = wezterm.font("Fira Code"),
+  window_decorations = "RESIZE",
+  color_scheme = "Catppuccin Mocha",
+  text_background_opacity = 0.8,
+  window_background_opacity = 0.8,
+  use_fancy_tab_bar = false,
+  automatically_reload_config = true,
+  hide_tab_bar_if_only_one_tab = true,
+  show_new_tab_button_in_tab_bar = false,
+  adjust_window_size_when_changing_font_size = false,
+  default_cursor_style = "BlinkingBar",
+  window_close_confirmation = "NeverPrompt",
+  window_padding = {
+    left = 35,
+    right = 30,
+    top = 20,
+    bottom = 20,
+  },
+  background = {
+    {
+      source = {
+        File = string.format("C:\\Users\\vladelaina\\.config\\wezterm\\images\\%s", BACKGROUND_IMAGE),
+      },
+      hsb = {
+        hue = 1.0,
+        saturation = 1.00,
+        brightness = 1,
+      },
     },
-    background = {
-        {
-            source = {
-                File = string.format("C:\\Users\\vladelaina\\.config\\wezterm\\%s", BACKGROUND_IMAGE),
-            },
-            hsb = {
-                hue = 1.0,
-                saturation = 1.00,
-                brightness = 1,
-            },
-        },
-        {
-            source = {
-                Color = "#282c35",
-            },
-            width = "100%",
-            height = "100%",
-            opacity = 0.55,
-        },
+    {
+      source = {
+        Color = "#282c35",
+      },
+      width = "100%",
+      height = "100%",
+      opacity = 0.55,
     },
-    default_prog = { "wsl.exe", "--cd", WSL_HOME },
-    initial_rows = 30,
-    initial_cols = 115,
-    keys = {
-        { key = 'q', mods = 'LEADER', action = wezterm.action.QuitApplication },
-        { key = 'h', mods = 'ALT|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-        { key = 'v', mods = 'ALT|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
-        { key = 'q', mods = 'CTRL', action = wezterm.action.CloseCurrentPane { confirm = false } },
-        { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Left' },
-        { key = 'RightArrow', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Right' },
-        { key = 'UpArrow', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Up' },
-        { key = 'DownArrow', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Down' },
-        { key = 't', mods = 'CTRL', action = wezterm.action.SpawnTab 'DefaultDomain' },
-        { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentTab { confirm = false } },
-        { key = 'n', mods = 'CTRL', action = wezterm.action.SpawnWindow },
-    }
+  },
+  default_prog = { "wsl.exe", "--cd", WSL_HOME },
+  initial_rows = 30,
+  initial_cols = 115,
+  keys = {
+    { key = 'q',          mods = 'LEADER',     action = wezterm.action.QuitApplication },
+    { key = 'h',          mods = 'ALT|SHIFT',  action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'v',          mods = 'ALT|SHIFT',  action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'q',          mods = 'CTRL',       action = wezterm.action.CloseCurrentPane { confirm = false } },
+    { key = 'LeftArrow',  mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'RightArrow', mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = 'UpArrow',    mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'DownArrow',  mods = 'SHIFT|CTRL', action = wezterm.action.ActivatePaneDirection 'Down' },
+    { key = 't',          mods = 'CTRL',       action = wezterm.action.SpawnTab 'DefaultDomain' },
+    { key = 'w',          mods = 'CTRL',       action = wezterm.action.CloseCurrentTab { confirm = false } },
+    { key = 'n',          mods = 'CTRL',       action = wezterm.action.SpawnWindow },
+  }
 }
 
 
